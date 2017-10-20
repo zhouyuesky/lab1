a4a7a79fcc33f432683c7fe122d9fea65b377d20
sunbai123456 <1264351015@qq.com>
2017-10-20 18:02:40 +0800

    last

--- src/test_for_every_dont_know/linkList.java
+++ src/test_for_every_dont_know/linkList.java
@@ -1,13 +1,14 @@
 package test_for_every_dont_know;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class linkList {
 	protected Node[] nodelst;
 	protected Node treeroot;
 	protected int count ;
 	public linkList() {
-		nodelst = new Node[2*Node.nodeLength];
+		nodelst = new Node[2*Node.getNodeLength()];
 		treeroot=null;
 		count=0;
 	}
@@ -56,7 +57,7 @@ public class linkList {
 		boolean findflag=false;
 		for(int i=0;i<count;i++)
 		{	
-			if(nodelst[i].data.equalsIgnoreCase(str))
+			if(nodelst[i].getData().equalsIgnoreCase(str))
 			{
 				findnode= nodelst[i];
 				findflag=true;
@@ -78,10 +79,10 @@ public class linkList {
 		{
 			tmp1=new Node(str1);
 			tmp2=new Node(str2);
-			tmp2.weightparent[Strlength(tmp2.weightparent)]=str1;
-			tmp2.weightnum[findindex(tmp2.weightparent,str1)]++;
-			tmp1.next[tmp1.number]=tmp2;
-			tmp1.number++;
+			tmp2.getWeightparent()[Strlength(tmp2.getWeightparent())]=str1;
+			tmp2.getWeightnum()[findindex(tmp2.getWeightparent(),str1)]++;
+			tmp1.getNext()[tmp1.getNumber()]=tmp2;
+			tmp1.setNumber(tmp1.getNumber() + 1);
 			nodelst[0]=tmp1;
 			nodelst[1]=(tmp2);
 			count=2;
@@ -95,23 +96,23 @@ public class linkList {
 				if(tmp2==null)//未找到str2，则新建str2指针并连接
 				{
 					tmp2=new Node(str2);
-					tmp2.weightparent[Strlength(tmp2.weightparent)]=str1;//创建父节点信息
-					tmp2.weightnum[findindex(tmp2.weightparent,str1)]++;//父节点权重+1
-					tmp1.next[tmp1.number]=tmp2;//连接子结点
-					tmp1.number++;//子结点计数+1
+					tmp2.getWeightparent()[Strlength(tmp2.getWeightparent())]=str1;//创建父节点信息
+					tmp2.getWeightnum()[findindex(tmp2.getWeightparent(),str1)]++;//父节点权重+1
+					tmp1.getNext()[tmp1.getNumber()]=tmp2;//连接子结点
+					tmp1.setNumber(tmp1.getNumber() + 1);//子结点计数+1
 					nodelst[count]=tmp2;//将新建的str2指针加入数组
 					count++;//节点计数+1
 				}
 				else{//找到str2，将str2指针连接进去
-					if(findindex(tmp2.weightparent,str1)==-1)//若str1不是str2父节点，创建连接
+					if(findindex(tmp2.getWeightparent(),str1)==-1)//若str1不是str2父节点，创建连接
 					{
-						tmp2.weightparent[Strlength(tmp2.weightparent)]=str1;//创建父节点信息
-						tmp2.weightnum[findindex(tmp2.weightparent,str1)]++;//父节点权重+1
-						tmp1.next[tmp1.number]=tmp2;//连接子结点
-						tmp1.number++;//子结点计数+1
+						tmp2.getWeightparent()[Strlength(tmp2.getWeightparent())]=str1;//创建父节点信息
+						tmp2.getWeightnum()[findindex(tmp2.getWeightparent(),str1)]++;//父节点权重+1
+						tmp1.getNext()[tmp1.getNumber()]=tmp2;//连接子结点
+						tmp1.setNumber(tmp1.getNumber() + 1);//子结点计数+1
 					}
 					else{//str1是str2父节点，权重+1
-						tmp2.weightnum[findindex(tmp2.weightparent,str1)]++;//权重+1
+						tmp2.getWeightnum()[findindex(tmp2.getWeightparent(),str1)]++;//权重+1
 					}
 					//tmp2.weightparent[Strlength(tmp2.weightparent)]=str1;
 				}
@@ -125,7 +126,7 @@ public class linkList {
 		int tmp=-1;
 		for(int i=0;i<count;i++)
 		{
-			if(nodelst[i].data.equalsIgnoreCase(str))
+			if(nodelst[i].getData().equalsIgnoreCase(str))
 			{
 				tmp=i;
 			}
@@ -151,25 +152,25 @@ public class linkList {
 		}
 		else
 		{
-			Node p,q;
+			Node p;
 			//Node visitlst[]=new Node[Node.nodeLength];
 			int top=0;
 			visitlst[0]=nodelst[indexOfNodeInlst(str1)];
 			p=visitlst[0];
-			p.visitflag=true;
+			p.setVisitflag(true);
 			do{										//寻找路径
-				if(p.visitcount<p.number)
+				if(p.getVisitcount()<p.getNumber())
 				{
-					if(p.next[p.visitcount].visitflag==false)
+					if(p.getNext()[p.getVisitcount()].isVisitflag()==false)
 					{
-						p.visitcount++;
-						p=p.next[p.visitcount-1];
+						p.setVisitcount(p.getVisitcount() + 1);
+						p=p.getNext()[p.getVisitcount()-1];
 						top++;
 						visitlst[top]=p;
-						p.visitflag=true;
+						p.setVisitflag(true);
 					}
 					else{
-						p.visitcount++;
+						p.setVisitcount(p.getVisitcount() + 1);
 					}
 					
 					
@@ -185,47 +186,50 @@ public class linkList {
 						return visitlst;
 					}
 				}
-			}while(!str2.equalsIgnoreCase(p.data));
+			}while(!str2.equalsIgnoreCase(p.getData()));
 			for(int i=top+1;i<visitlst.length;i++)//清空非路径部分
 			{
 				visitlst[i]=null;
 			}
 			for(int i=0;i<Strlength(nodelst);i++)//恢复访问标志为false，为下次访问做准备
 			{
-				nodelst[i].visitflag=false;
-				nodelst[i].visitcount=0;
+				nodelst[i].setVisitflag(false);;
+				nodelst[i].setVisitcount(0);
 			}
 		return visitlst;
 		}
 	}//searchroad
 	public void displaytree()//功能2 展示图
 	{
-		Node tmp;
 		String type = "jpg";
 		GraphViz gv = new GraphViz();
-	    gv.addln(gv.start_graph());
+	    gv.addln(gv.startgraph());
 	    for(int i=0;i<count;i++)
 	    {
-	    	for(int j=0;j<nodelst[i].number;j++)
+	    	for(int j=0;j<nodelst[i].getNumber();j++)
 	    	{
 	    		//if(nodelst[i].!=0)
 	    		//{
-	    			gv.addlnlabel(nodelst[i].data+"->"+nodelst[i].next[j].data,""+nodelst[i].next[j].weightnum[findindex(nodelst[i].next[j].weightparent,nodelst[i].data)]);
+	    			gv.addlnlabel(nodelst[i].getData()+"->"+nodelst[i].getNext()[j].getData(),""+nodelst[i].getNext()[j].getWeightnum()[findindex(nodelst[i].getNext()[j].getWeightparent(),nodelst[i].getData())]);
 	    		//}
 	    	}
 	    }
-	    gv.addln(gv.end_graph());
-	    File out = new File("E:\\ProgramLanguageOfAll\\myeclipse\\workplace\\test_for_every_dont_know\\out." + type);    // Windows
-	    gv.writeGraphToFile( gv.getGraph( gv.getDotSource(), type ), out );
+	    gv.addln(gv.endgraph());
+	    File out = new File("F:\\EclipseWorkspace\\test_for_every_dont_know\\out." + type);    // Windows
+	    //System.out.println(gv.getDotSource());
+	    byte[] temp=gv.getGraph( gv.getDotSource(), type );
+	    //System.out.println(temp);
+	    gv.writeGraphToFile( temp, out );
 	}
 	public void searchBridgeWords(String str1,String str2)//功能3查询桥接词
 	{
-		int visitnodenum=2*Node.nodeLength,top = 0,findin1,findin2;
+		int visitnodenum=2*Node.getNodeLength(),top = 0,findin1,findin2;
 		Node visitlst[]=new Node[visitnodenum];
-		for(int i=0;i<visitnodenum;i++)
-		{
-			visitlst[i]=null;
-		}
+		Arrays.fill(visitlst,null);
+//		for(int i=0;i<visitnodenum;i++)
+//		{
+//			visitlst[i]=null;
+//		}
 		findin1=indexOfNodeInlst(str1);
 		findin2=indexOfNodeInlst(str2);
 		if(findin1==-1&&findin2==-1)
@@ -261,7 +265,7 @@ public class linkList {
 			System.out.println("The bridge words from "+ str1+ " to "+ str2+" are: ");
 			for(int i=1;i<top-1;i++)
 			{
-				System.out.print(visitlst[i].data);
+				System.out.print(visitlst[i].getData());
 				if(i!=top-2)
 				{
 					System.out.print(",");
@@ -279,18 +283,20 @@ public class linkList {
 		String regex="[^\\p{Alpha}]+";
 		String[] strspilt=strinput.split(regex);
 		String[] strout=new String[(int)(10*strspilt.length)];
-		for(int i=0;i<strout.length;i++){
-			strout[i]="";
-		}
-		String[] strtmp=new String[Node.nodeLength];
-		for(int i=0;i<strtmp.length;i++){
-			strtmp[i]="";
-		}
-		Node[] strnodetmp=new Node[Node.nodeLength];
-		for(int i=0;i<strnodetmp.length;i++)
-		{
-			strnodetmp[i]=null;
-		}
+		Arrays.fill(strout,"");
+//		for(int i=0;i<strout.length;i++){
+//			strout[i]="";
+//		}
+		String[] strtmp=new String[Node.getNodeLength()];
+		Arrays.fill(strtmp,"");
+//		for(int i=0;i<strtmp.length;i++){
+//			strtmp[i]="";
+//		}
+		Node[] strnodetmp=new Node[Node.getNodeLength()];
+//		for(int i=0;i<strnodetmp.length;i++)
+//		{
+//			strnodetmp[i]=null;
+//		}
 		strout[0]=strspilt[0];
 		int nodetmplength;
 		for(int i=0;i<strspilt.length-1;i++)
@@ -306,9 +312,9 @@ public class linkList {
 			{
 				for(int j=0;strnodetmp[j]!=null;j++)
 				{
-					strtmp[j]=strnodetmp[j].data;
+					strtmp[j]=strnodetmp[j].getData();
 				}
-				for(int k=1;strtmp[k]!="";k++)//添加词语
+				for(int k=1;!"".equals(strtmp[k]);k++)//添加词语
 				{
 					strout[Strlength(strout)]=strtmp[k];
 				}
@@ -350,7 +356,7 @@ public class linkList {
 		boolean flag=true;
 		for(int i=0;i<Strlength(nodelst);i++)
 		{
-			if(nodelst[i].visitflag==false)
+			if(nodelst[i].isVisitflag()==false)
 			{
 				flag=false;
 				break;
@@ -361,15 +367,15 @@ public class linkList {
 	}
 	public void shortestroad(String str1,String str2)//功能需求5：计算两个单词之间的最短路径
 	{
-		Node resultlst[][] = new Node[Node.nodeLength][];
-		for(int i=0;i<Node.nodeLength;i++)
+		Node resultlst[][] = new Node[Node.getNodeLength()][];
+		for(int i=0;i<Node.getNodeLength();i++)
 		{
-			resultlst[i]=new Node[2*Node.nodeLength];
+			resultlst[i]=new Node[2*Node.getNodeLength()];
 		}
-		Node tmpnode[] = new Node[2*Node.nodeLength],minroad[][]=new Node[Node.nodeLength][];
-		for(int i=0;i<Node.nodeLength;i++)
+		Node tmpnode[] = new Node[2*Node.getNodeLength()],minroad[][]=new Node[Node.getNodeLength()][];
+		for(int i=0;i<Node.getNodeLength();i++)
 		{
-			minroad[i]=new Node[2*Node.nodeLength];
+			minroad[i]=new Node[2*Node.getNodeLength()];
 		}
 		int count=0;
 		int maxofallroads;
@@ -383,10 +389,10 @@ public class linkList {
 				{
 					for(int j=1;j<Strlength(resultlst[i])-1;j++)
 					{
-						resultlst[i][j].selfvisitcount++;
-						if(resultlst[i][j].number==resultlst[i][j].selfvisitcount)
+						resultlst[i][j].setSelfvisitcount(resultlst[i][j].getSelfvisitcount()+1);
+						if(resultlst[i][j].getNumber()==resultlst[i][j].getSelfvisitcount())
 						{
-							resultlst[i][j].visitflag=true;
+							resultlst[i][j].setVisitflag(true);
 						}
 						else{
 							break;
@@ -394,7 +400,7 @@ public class linkList {
 					}//break;
 				}
 				allnodevisitflag=isallvisited();//获得全体访问标志
-				tmpnode=new Node[2*Node.nodeLength];
+				tmpnode=new Node[2*Node.getNodeLength()];
 				if(allnodevisitflag==false)//并非全体都被访问，则进行路径查找
 				{
 					tmpnode=searchroad(str1,str2,tmpnode);//调用路径搜索函数searchroad获得路径数组
@@ -403,16 +409,16 @@ public class linkList {
 				{
 					count++;
 					resultlst[count]=tmpnode;
-					tmpnode=new Node[2*Node.nodeLength];
+					tmpnode=new Node[2*Node.getNodeLength()];
 				}
 				else{
 					allnodevisitflag=true;//路径不存在，将全体访问标志改为true，表示访问结束，此时结束循环
 				}
 				for(int i=0;i<Strlength(nodelst);i++)
 				{
-					nodelst[i].visitcount=0;
-					nodelst[i].visitflag=false;
-					nodelst[i].selfvisitcount=0;
+					nodelst[i].setVisitcount(0);
+					nodelst[i].setVisitflag(false);
+					nodelst[i].setSelfvisitcount(0);
 				}
 			}while(allnodevisitflag==false);
 			maxofallroads = count+1;//冒泡排序
@@ -445,7 +451,7 @@ public class linkList {
 				{
 					for(int j=0;j<Strlength(resultlst[i]);j++)
 					{
-						System.out.print(resultlst[i][j].data);
+						System.out.print(resultlst[i][j].getData());
 						if(j!=Strlength(resultlst[i])-1)
 						{
 							System.out.print("->");
@@ -469,15 +475,16 @@ public class linkList {
 		}
 		for(int i=0;i<Strlength(nodelst);i++)
 		{
-			nodelst[i].visitcount=0;
-			nodelst[i].visitflag=false;
-			nodelst[i].selfvisitcount=0;
+			
+			nodelst[i].setVisitcount(0);
+			nodelst[i].setVisitflag(false);
+			nodelst[i].setSelfvisitcount(0);
 		}
 		allnodevisitflag=false;
 		count=0;
-		for(int i=0;i<Node.nodeLength;i++)
+		for(int i=0;i<Node.getNodeLength();i++)
 		{
-			resultlst[i]=new Node[2*Node.nodeLength];
+			resultlst[i]=new Node[2*Node.getNodeLength()];
 		}
 		return;
 		//System.out.println("================Task 5================");
@@ -494,40 +501,40 @@ public class linkList {
 		}
 		Node p,q;
 		boolean allvisitofnodelst=false;
-		Node visitlst[]=new Node[2*Node.nodeLength];
+		Node visitlst[]=new Node[2*Node.getNodeLength()];
 		int top=0;
 		visitlst[0]=randomroot;
 		p=visitlst[0];
-		randomVisitRoad[Strlength(randomVisitRoad)]=p.data;
-		p.selfvisitcount++;
-		if(p.selfvisitcount==p.number)
+		randomVisitRoad[Strlength(randomVisitRoad)]=p.getData();
+		p.setSelfvisitcount(p.getSelfvisitcount() + 1);
+		if(p.getSelfvisitcount()==p.getNumber())
 		{
-			p.visitflag=true;
+			p.setVisitflag(true);
 		}
 		do{
 			if(!allvisitofnodelst)
 			{
-				randinttmp=(int)(Math.random()*p.number);
-				if(p.number==0)
+				randinttmp=(int)(Math.random()*p.getNumber());
+				if(p.getNumber()==0)
 				{
 					allvisitofnodelst=true;
 				}
-				else if(p.next[randinttmp].visitflag==false)
+				else if(p.getNext()[randinttmp].isVisitflag()==false)
 				{
-					p.visitcount++;
-					p=p.next[randinttmp];
-					randomVisitRoad[Strlength(randomVisitRoad)]=p.data;
-					p.selfvisitcount++;
-					if(p.selfvisitcount==p.number)
+					p.setVisitcount(p.getVisitcount() + 1);
+					p=p.getNext()[randinttmp];
+					randomVisitRoad[Strlength(randomVisitRoad)]=p.getData();
+					p.setSelfvisitcount(p.getSelfvisitcount() + 1);
+					if(p.getSelfvisitcount()==p.getNumber())
 					{
-						p.visitflag=true;
+						p.setVisitflag(true);
 					}
 					top++;
 					visitlst[top]=p;
 				}
 				else{
-					p=p.next[randinttmp];
-					randomVisitRoad[Strlength(randomVisitRoad)]=p.data;
+					p=p.getNext()[randinttmp];
+					randomVisitRoad[Strlength(randomVisitRoad)]=p.getData();
 					allvisitofnodelst=true;
 				}
 			}
@@ -547,9 +554,9 @@ public class linkList {
 		}
 		for(int i=0;i<Strlength(nodelst);i++)
 		{
-			nodelst[i].visitcount=0;
-			nodelst[i].visitflag=false;
-			nodelst[i].selfvisitcount=0;
+			nodelst[i].setVisitcount(0);
+			nodelst[i].setVisitflag(false);
+			nodelst[i].setSelfvisitcount(0);
 		}
 		
 	}//randomWalk
